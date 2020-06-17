9680e7366a6985c6313f9350413175dfed5ad12c
Kjuman Enobikto <gekyakibo@usako.net>
2020-06-17 21:43:21 +0900

    定期投稿試験

--- SO2MI/Events.py
+++ SO2MI/Events.py
@@ -109,8 +109,7 @@ def funcEvent():
                 eventComeText.append(evtxt)
 
         eventText = eventHeldText + eventComeText
-        with open("event.txt", "w", encoding="utf-8_sig") as evtxtarr:
-            evtxtarr.write(str(eventText))
+        return eventText
     except:
         traceback.print_exc()
         return False

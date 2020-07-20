239c77dada7ae9749ce622382727bc6c8e193b02
QmanEnobikto <30486982+Qman11010101@users.noreply.github.com>
2020-07-20 15:18:56 +0900

    定期実行システム一旦潰してとりあえず再開

--- main.py
+++ main.py
@@ -42,39 +42,39 @@ stream = "https://stream.twitter.com/1.1/statuses/filter.json"
 tweet = "https://api.twitter.com/1.1/statuses/update.json"
 
 if __name__ == "__main__":
-    # 定期実行関数定義
-    def regexc():
-        # 取得してツイートするもの(ツイートは1度に5つまでを目安)
-        # ・イベント情報
-        # ・ランキング上位数人
-        # ・月末月始の優待券使用奨励
-        # 定期ツイートは8時/12時/16時/20時
-        while True:
-            print("定期実行システム作動中")
-            asyncio.sleep(300) # 5分ごとにチェック
+    # # 定期実行関数定義
+    # def regexc():
+    #     # 取得してツイートするもの(ツイートは1度に5つまでを目安)
+    #     # ・イベント情報
+    #     # ・ランキング上位数人
+    #     # ・月末月始の優待券使用奨励
+    #     # 定期ツイートは8時/12時/16時/20時
+    #     while True:
+    #         print("定期実行システム作動中")
+    #         asyncio.sleep(300) # 5分ごとにチェック
 
-            # 時間取得
-            hour = datetime.datetime.now().hour
-            minu = datetime.datetime.now().minute
+    #         # 時間取得
+    #         hour = datetime.datetime.now().hour
+    #         minu = datetime.datetime.now().minute
 
-            # 分岐
-            if hour == 8 and 0 <= minu <= 5:
-                logger("8時の処理を開始します", "debug")
-            elif hour == 12 and 0 <= minu <= 5:
-                logger("12時の処理を開始します", "debug")
-            elif hour == 16 and 0 <= minu <= 5:
-                logger("16時の処理を開始します", "debug")
-            elif hour == 20 and 0 <= minu <= 5:
-                logger("20時の処理を開始します", "debug")
-                # イベント投稿(1日1回)
-                res = funcEvent()
-                if len(res):
-                    for post in range(min(5, len(res))):
-                        content = {"status": res[post]}
-                        resPost = session.post(tweet, params=content)
-                        asyncio.sleep(5)
-            else:
-                pass
+    #         # 分岐
+    #         if hour == 8 and 0 <= minu <= 5:
+    #             logger("8時の処理を開始します", "debug")
+    #         elif hour == 12 and 0 <= minu <= 5:
+    #             logger("12時の処理を開始します", "debug")
+    #         elif hour == 16 and 0 <= minu <= 5:
+    #             logger("16時の処理を開始します", "debug")
+    #         elif hour == 20 and 0 <= minu <= 5:
+    #             logger("20時の処理を開始します", "debug")
+    #             # イベント投稿(1日1回)
+    #             res = funcEvent()
+    #             if len(res):
+    #                 for post in range(min(5, len(res))):
+    #                     content = {"status": res[post]}
+    #                     resPost = session.post(tweet, params=content)
+    #                     asyncio.sleep(5)
+    #         else:
+    #             pass
 
     # メイン関数定義
     def mainfunc():
@@ -102,13 +102,13 @@ if __name__ == "__main__":
                                 logger("投稿完了しました")
                                 pass
 
-    # 定期実行スレッドのデーモン化
-    th = threading.Thread(target=regexc, name="th", args=())
-    th.setDaemon(True)
+    # # 定期実行スレッドのデーモン化
+    # th = threading.Thread(target=regexc, name="th", args=())
+    # th.setDaemon(True)
 
     # セッション生成
     session = OAuth1Session(consumerKey, consumerSecret, accessToken, accessTokenSecret)
 
     # 実行
     mainfunc()
-    th.start()
+    # th.start()
